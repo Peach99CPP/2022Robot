6e783770ce9cfdd650052000e99790ce1c0df92f
peach <1831427532@qq.com>
2022-09-12 16:30:12 +0800

    today finish

--- MDK-ARM/motor/usmart_config.d
+++ MDK-ARM/motor/usmart_config.d
@@ -77,7 +77,7 @@ motor\usmart_config.o: ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cms
 motor\usmart_config.o: ../Module/general.h
 motor\usmart_config.o: ../Module/QR_code.h
 motor\usmart_config.o: ../Driver/avoid_obs.h
-motor\usmart_config.o: ..\Appliciation\general_interface.h
+motor\usmart_config.o: ../Appliciation/general_interface.h
 motor\usmart_config.o: ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h
 motor\usmart_config.o: ../TASK/read_status.h 
 motor\usmart_config.o: ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h
