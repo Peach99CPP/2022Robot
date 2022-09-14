2e9f0dbdfd42c7bd86452b11606b7370d1fd00d4
peach <1831427532@qq.com>
2022-09-14 16:29:34 +0800

    add description

--- Appliciation/general_interface.c
+++ Appliciation/general_interface.c
@@ -2,7 +2,7 @@
  * @Author: peach 1831427532@qq.com
  * @Date: 2022-09-05 14:16:44
  * @LastEditors: peach 1831427532@qq.com
- * @LastEditTime: 2022-09-14 15:57:51
+ * @LastEditTime: 2022-09-14 16:27:06
  * @FilePath: \MDK-ARMd:\robot\robot\Appliciation\general_interface.c
  * @Description:
  *
@@ -11,7 +11,7 @@
 
 #include "general_interface.h"
 /**
- * @description:
+ * @description: 通过调用红外俩计算板子数量
  * @param {int} id使用哪个红外
  * @param {int} num 数多少个板子
  * @param {int} speed以什么速度运行
