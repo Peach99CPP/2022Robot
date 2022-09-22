49c798463000b323017c72976b3405ef380148c5
peach <1831427532@qq.com>
2022-09-22 17:05:27 +0800

    当前红场参数无明显问题

--- Appliciation/general_interface.c
+++ Appliciation/general_interface.c
@@ -2,7 +2,7 @@
  * @Author: peach 1831427532@qq.com
  * @Date: 2022-09-05 14:16:44
  * @LastEditors: peach 1831427532@qq.com
- * @LastEditTime: 2022-09-22 16:48:35
+ * @LastEditTime: 2022-09-22 17:01:08
  * @FilePath: \MDK-ARMd:\robot\robot\Appliciation\general_interface.c
  * @Description:
  *
@@ -14,7 +14,7 @@
 #define Wait_Dealy_MAX 30000
 #define Line_Type 1
 #define Encoder_Type 2
-#define YuanPanDelay 16 * 1000
+#define YuanPanDelay 18 * 1000
 /**
  * @description: 通过调用红外俩计算板子数量
  * @param {int} id使用哪个红外
